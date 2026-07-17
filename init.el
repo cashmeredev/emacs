c8dc386b881d725d5f3df16bb8c96abc1b0a1d43
cashmere <cashmere@autistici.org>
2026-07-17 06:55:11 +0200

    limit elpaca pulls

--- init.el
+++ init.el
@@ -5,6 +5,8 @@
 (defvar elpaca-core-date
   (list (string-to-number (format-time-string "%Y%m%d" (or emacs-build-time (current-time))))))
 
+(defvar elpaca-queue-limit 8)
+
 ;; Elpaca bootstrap
 (defvar elpaca-installer-version 0.12)
 (defvar elpaca-directory (expand-file-name "elpaca/" user-emacs-directory))
