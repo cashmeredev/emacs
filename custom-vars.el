06bf588021942f73cbb44fec816f1ea57a20db9c
cashmere <cashmere@autistici.org>
2026-09-12 14:45:48 +0200

    x12: 2026-09-12T14:45:48+02:00

--- custom-vars.el
+++ custom-vars.el
@@ -5,7 +5,8 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("d43860349c9f7a5b96a090ecf5f698ff23a8eb49cd1e5c8a83bb2068f24ea563"
+   '("01f347a923dd21661412d4c5a7c7655bf17fb311b57ddbdbd6fce87bd7e58de6"
+     "d43860349c9f7a5b96a090ecf5f698ff23a8eb49cd1e5c8a83bb2068f24ea563"
      default))
  '(helm-minibuffer-history-key "M-p")
  '(package-selected-packages
@@ -56,7 +57,20 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(org-block ((t (:inherit fixed-pitch))))
+ '(org-block-begin-line ((t (:inherit (shadow fixed-pitch)))))
+ '(org-block-end-line ((t (:inherit (shadow fixed-pitch)))))
+ '(org-checkbox ((t (:inherit fixed-pitch))))
+ '(org-code ((t (:inherit (shadow fixed-pitch)))))
  '(org-document-title ((t (:height 1.6))))
+ '(org-drawer ((t (:inherit fixed-pitch))))
+ '(org-indent ((t (:inherit (org-hide fixed-pitch)))))
+ '(org-meta-line ((t (:inherit (font-lock-comment-face fixed-pitch)))))
+ '(org-property-value ((t (:inherit fixed-pitch))))
+ '(org-special-keyword ((t (:inherit (font-lock-comment-face fixed-pitch)))))
+ '(org-table ((t (:inherit fixed-pitch))))
+ '(org-tag ((t (:inherit fixed-pitch))))
+ '(org-verbatim ((t (:inherit (shadow fixed-pitch)))))
  '(outline-1 ((t (:height 1.25))))
  '(outline-2 ((t (:height 1.2))))
  '(outline-3 ((t (:height 1.2))))
